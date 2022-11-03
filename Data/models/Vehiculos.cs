0d35f110eb40f62756bd9cac54c503411784cee9
RichardGarcia01 <rg0329221@gmail.com>
2022-11-03 12:56:57 -0400

    1

--- Data/models/Vehiculos.cs
+++ Data/models/Vehiculos.cs
@@ -7,7 +7,16 @@ public class vehiculos
     public int vehiculoID {get; set; }
     public char Marca {get; set;} 
     public char Modelo {get; set;}
-    public  int año {get; set;}
+
+    private int año1;
+
+    public int Getaño => año1;
+
+    public void Setaño(int value)
+    {
+        año1 = value;
+    }
+
     public char Color {get; set;}
 }
 
