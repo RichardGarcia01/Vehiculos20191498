034c4260479d045196bbc855411687213f23b6d6
RichardGarcia01 <rg0329221@gmail.com>
2022-11-03 12:28:19 -0400

    0.2

---
+++ Data/models/Vehiculos.cs
@@ -0,0 +1,14 @@
+namespace Vehiculos2019_1498;
+
+public class vehiculos 
+{
+    
+
+    public int vehiculoID {get; set; }
+    public string Marca {get; set;} = null!;
+    public string Modelo {get; set;}= null!;
+    public  double año {get; set;}
+    public string Color {get; set;}= null!;
+}
+
+
